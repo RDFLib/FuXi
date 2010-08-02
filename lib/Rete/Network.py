3e93c552854483fe5908a476e25a375aec68d37f
Chimezie Ogbuji <chimezie@gmail.com>
2010-08-01 20:12:28 -0400

    buggy line is still there above the fixed one:

--- lib/Rete/Network.py
+++ lib/Rete/Network.py
@@ -442,7 +442,6 @@ class ReteNetwork:
                 node.memories[RIGHT_MEMORY].reset()
         self.justifications = {}
         self.proofTracers = {}
-        self.inferredFacts = newinferredFacts and newinferredFacts or Graph()
         self.inferredFacts = newinferredFacts if newinferredFacts is not None else Graph()
         self.workingMemory = set()
         self._resetinstanciationStats()        
