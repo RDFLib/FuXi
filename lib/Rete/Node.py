9d981cf1038b527a7c0e9187aec21f179059f17d
Chimezie Ogbuji <chimezie@gmail.com>
2010-10-24 00:44:25 -0400

    ensure memory is not duplicated

--- lib/Rete/Node.py
+++ lib/Rete/Node.py
@@ -5,7 +5,11 @@ class Node(object):
     """
     A node in a Rete network.  Behavior between Alpha and Beta (Join) nodes
     """
+    def updateDescendentMemory(self,memory):
+        if memory.successor not in [mem.successor for mem in self.descendentMemory]:
+            self.descendentMemory.append(memory)
+        
     def connectToBetaNode(self,betaNode,position):
         from BetaNode import BetaNode, LEFT_MEMORY, RIGHT_MEMORY, PartialInstanciation
-        self.descendentMemory.append(betaNode.memories[position])
+        self.updateDescendentMemory(betaNode.memories[position])
         self.descendentBetaNodes.add(betaNode)        
