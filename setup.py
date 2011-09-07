2307e4d8373827837ab813d21d8b7c0f149c6982
Chimezie Ogbuji <chimezie@gmail.com>
2011-09-07 17:24:03 -0400

    - bumped version to 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools  import setup
 setup(name="FuXi",
-      version="1.2",
+      version="1.3",
       description="An OWL / N3-based in-memory, logic reasoning system for RDF",
       author="Chime Ogbuji",
       author_email="chimezie@gmail.com",
