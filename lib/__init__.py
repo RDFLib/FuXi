8b6e5c0df4b24bf9c35ce449fe40aa2aa7121973
Graham Higgins <gjh-github@bel-epa.com>
2012-11-03 02:54:52 +0000

    Temporarily added a debug fn and registered the "text/xml" mimetype as an RDF/XML parser.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,12 +1,14 @@
+import logging
 import rdflib
 rdflib.plugin.register('sparql', rdflib.query.Processor,
                       'rdfextras.sparql.processor', 'Processor')
 rdflib.plugin.register('sparql', rdflib.query.Result,
                       'rdfextras.sparql.query', 'SPARQLQueryResult')
+rdflib.plugin.register('text/xml', rdflib.parser.Parser,
+                'rdflib.plugins.parsers.rdfxml', 'RDFXMLParser')
 
 
-def _debug(*args, **kw):
-    import logging
+def debug(*args, **kw):
     logging.basicConfig(level=logging.ERROR, format="%(message)s")
     logger = logging.getLogger(__name__)
     logger.debug(*args, **kw)
